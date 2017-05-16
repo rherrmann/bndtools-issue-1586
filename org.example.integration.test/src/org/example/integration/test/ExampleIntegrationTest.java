7a489e8b9cdd6d7e82e9f2c78a39bc3d6055f2c6
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2017-05-16 21:47:24 +0200

    Integration test example

---
+++ org.example.integration.test/src/org/example/integration/test/ExampleIntegrationTest.java
@@ -0,0 +1,18 @@
+package org.example.integration.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExampleIntegrationTest {
+	
+	@Test
+	public void test1() throws Exception {
+		assertTrue(false);
+	}
+
+	@Test
+	public void test2() throws Exception {
+		assertTrue(true);
+	}
+}
